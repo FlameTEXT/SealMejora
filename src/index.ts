c28e88583d80d576d4e6aafee053de0fb5554088
Verplitic <57583509+Verplitic@users.noreply.github.com>
2023-02-13 16:02:12 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 extension.Register({
-  Name: "Tester Ext",
+  Name: "tester",
   Author: "me",
   Version: "0.0.1"
 });
@@ -8,7 +8,18 @@ let cmdInfo = {
   Name: "say",
   Help: "No help",
   NeedHelp: true
-}
+};
+
 extension.AddCommand(cmdInfo, cmd => {
   cmd.reply("hello!");
+});
+
+let anotherCmdInfo = {
+  Name: "bye",
+  Help: "No help",
+  NeedHelp: false
+}
+
+extension.AddCommand(anotherCmdInfo, cmd => {
+  cmd.reply("bye!");
 });
